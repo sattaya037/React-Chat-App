cc5315f35d1ddaa9dd81e7bd2bb54aa9af2613bb
sattaya rakjan <contea2540@hotmail.com>
2019-05-27 18:57:15 +0700

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,7 +4,7 @@ import { USER_CONNECTED, LOGOUT,VERIFY_USER } from '../Event'
 import LoginForm from './LoginForm'
 import ChatContainer from './chats/ChatContainer'
 
-const socketUrl = "/"
+const socketUrl = "http://localhost:323"
 export default class Layout extends Component {
 	
 	constructor(props) {
